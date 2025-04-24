a529a97e893ee4495a92d890fd189a4ac1ea19e3
Crunchy <quinn.jp@gmail.com>
2025-04-24 14:01:12 -0400

    Parse the first batch of questions into the Question List

--- LookUps/QuestionTextConstants.cs
+++ LookUps/QuestionTextConstants.cs
@@ -5,4 +5,5 @@ public static class QuestionTextConstants
     public const string MULTIPLE_CHOICE = "multiple_choice_question";
     public const string FILL_IN_THE_BLANK = "fill_in_the_blank";
     public const string MATCH_TERMS = "matching_terms";
+    public const string CORRECT_ANSWER = "Answer";
 }
--- Parse/ParseTextToQuestions.cs
+++ Parse/ParseTextToQuestions.cs
@@ -6,7 +6,8 @@ namespace Parse;
 
 public class ParseTextToQuestions
 {
-    QuestionTypes questionType;
+    private QuestionTypes _questionType;
+    private Question? _question;
     
     /// <summary>
     ///  Parse the text into a list of questions. 
@@ -25,10 +26,94 @@ public class ParseTextToQuestions
         // todo: this first pass is for multiple choice only, we will update this to handle the other question types later
         foreach (var line in lines)
         {
+            if(line.StartsWith("**") && line.EndsWith("**"))
+            {
+                // this is a question type
+                SetQuestionType(line);
+                continue;
+            }
             
+            if (line.StartsWith(">"))
+            {
+                // this is a new Question, add the last question and answers to the list 
+                if (_question != null)
+                {
+                    questions.Add(_question);
+                }
+                
+                // intialize the next Question
+                _question = new Question();
+                
+                _question.QuestionType = _questionType.ToText();
+                // todo: change this to set the points dynamically if required
+                _question.PointsPossible = 1;
+                
+                SetQuestionText(line);
+            }
+            else if (line.StartsWith('='))
+            {
+                // this is a possible answer, so add to the answer section
+                SetAnswerText(line);
+            }
+            else if (line.StartsWith(QuestionTextConstants.CORRECT_ANSWER))
+            {
+                SetCorrectAnswer(line);
+            }
         }
 
         return questions;
+    }
+
+    private void SetCorrectAnswer(string line)
+    {
+        // Correct answers are prepended with the string "Answer"
+        // Extract the correct answer text
+        int index;
+        if (line.Contains(")"))
+        {
+            // there is whitespace between the parenthesis and the start of the answer
+            index = line.IndexOf(')') + 2;
+        }
+        else
+        {
+            // there is whitespace between the parenthesis and the start of the answer
+            index = line.IndexOf(':') + 2;
+        }
+        var correctAnswerText = line.Substring(index).Trim();
+
+        // Set the CorrectAnswer property of the question
+        _question.CorrectAnswer = correctAnswerText;
+
+        // Find the matching answer and set its IsCorrect property to true
+        var correctAnswer = _question.Answers?.FirstOrDefault(x => x.Text.Contains(correctAnswerText));
+        if (correctAnswer != null)
+        {
+            correctAnswer.IsCorrect = true;
+        }
+        
+    }
+
+    private void SetAnswerText(string line)
+    {
+        // the answer text starts with a letter and a left parenthesis. We can strip these characters
+        // as they are not used in the XML document
+        var startIndex = line.IndexOf(')');
+        var answerText = line.Substring(startIndex + 1).TrimStart();
+
+        _question.Answers ??= new List<Answer>();
+        
+        _question.Answers.Add(new Answer
+        {
+            Text = answerText
+        });
+    }
+
+    private void SetQuestionText(string line)
+    {
+        // remove the leading > and trailing whitespace
+        var text = line.TrimStart('>').TrimStart();
+        
+        _question.Text = text;
     }
 
     private void SetQuestionType(string text)
@@ -36,7 +121,7 @@ public class ParseTextToQuestions
         // if the text starts and ends with two asterisks, it will contain the question type
         var questionTypeText = text.TrimStart('*').TrimEnd('*');
 
-        questionType = questionTypeText switch
+        _questionType = questionTypeText switch
         {
             QuestionTextConstants.MULTIPLE_CHOICE => QuestionTypes.MultipleChoice,
             QuestionTextConstants.FILL_IN_THE_BLANK => QuestionTypes.FillInTheBlank,
@@ -67,4 +152,18 @@ public class ParseTextToQuestions
 
         return result;
     }
+
+    private string RemoveMetaText(string line)
+    {
+        var trimmedText = RemoveMetaText(line);
+
+        string text = string.Empty;
+        if (line.StartsWith("**") && line.EndsWith("**"))
+        {
+            text = line.TrimStart('*').TrimEnd('*');
+        }
+        
+        return text;
+    }
+    
 }
